db983fdccddfc6421724b158822402933b84e66d
jkxruby <jkxruby@gmail.com>
2017-11-30 17:39:21 +0800

    实现一个矢量函数-模拟数值类型

--- chapter1.py
+++ chapter1.py
@@ -47,7 +47,61 @@ for card in sorted(deck, key = spades_high):
     print(card)
 # >>> 结果是2，2，2，2，3，3，3，3...这样排序下去的
 
-'''
+
 #------------------------------------------#
+# 一个简单的二维向量 类
+from math import hypot
+class Vector:
+    def __init__(self,x=0,y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vecotr(%s,%s)' %(self.x, self.y)
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.x + other.y
+        return Vector(x, y)
+
+    def __mul__(self, other):
+        return Vector(self.x * scalar, self.y * scalar)
+#-------------------------------------------#
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
